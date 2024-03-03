cb1d2f2d9436cd08900fb3de7261f28ed22436dc
nawel laouini <nawellaouini210@gmail.com>
2024-03-03 13:44:01 +0100

    Added some Tiwggy goodness

--- src/Controller/VinlyController.php
+++ src/Controller/VinlyController.php
@@ -1,14 +1,26 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function symfony\Component\String\u;
 
- class VinlyController{
+ class VinlyController  extends AbstractController {
     #[Route('/')]
-    public function homepage(){
-        return  new Response('Titre: PD and Jams');
+    public function homepage() :Response{
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+        return  $this->render('vinly/homepage.html.twig',[
+            'title'=>'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
     #[Route('/browse/{slug}')]
     public function browse( string $slug): Response
