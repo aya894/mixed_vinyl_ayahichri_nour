a343ee675ee4670baae5870b1e3acab67bf8ef90
Aya Hichri <ayahichri2022@gmail.com>
2024-03-12 09:52:05 +0100

    Never gonna let you go...

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Psr\Log\LoggerInterface;
+
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'], name: 'api_songs_get_one')]
     public function getSong(int $id, LoggerInterface $logger): Response
     {
         // TODO query the database
@@ -18,10 +19,11 @@ class SongController extends AbstractController
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
-        
+
         $logger->info('Returning API response for song {song}', [
             'song' => $id,
         ]);
+
         return $this->json($song);
     }
 }
